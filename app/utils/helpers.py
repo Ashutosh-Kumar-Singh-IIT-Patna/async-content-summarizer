e9dd517cc83b44be4975b0000df9b2f3677e8f5b
Ashutosh Kumar Singh <ashutoshksingh2003@gmail.com>
2026-01-08 02:03:38 +0530

    corrections

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -10,6 +10,8 @@ logger = logging.getLogger(__name__)
 
 
 def generic_retry(max_attempts=3, delay=5):
+    """Decorator to retry a function on failure"""
+
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
@@ -28,14 +30,17 @@ def generic_retry(max_attempts=3, delay=5):
 
 
 def hash_content(text):
+    """Generate SHA256 hash of text content for caching"""
     return hashlib.sha256(text.encode()).hexdigest()
 
 
+# Database retry configuration
 MAX_RETRIES = 3
 RETRY_WAIT_SECONDS = 2
 
 
 def retry_on_pgdb_exception(func):
+    """Decorator to retry database operations on connection errors"""
 
     def wrapper(*args, **kwargs):
         retries = 0
@@ -45,6 +50,7 @@ def retry_on_pgdb_exception(func):
             except (OperationalError, Psycopg2OperationalError) as e:
                 logger.error("OperationalError: %s", e)
                 retries += 1
+
                 if retries < MAX_RETRIES:
                     logger.info("Retrying... (%d/%d)", retries, MAX_RETRIES)
                     time.sleep(RETRY_WAIT_SECONDS)
@@ -64,16 +70,12 @@ def retry_on_pgdb_exception(func):
 
 @retry_on_pgdb_exception
 def write_to_pgdb(obj):
+    """Add object to database and commit"""
     db.session.add(obj)
     db.session.commit()
 
 
 @retry_on_pgdb_exception
-def delete_from_pgdb(obj):
-    db.session.delete(obj)
-    db.session.commit()
-
-
-@retry_on_pgdb_exception
 def commit_pgdb():
+    """Commit current database session"""
     db.session.commit()
